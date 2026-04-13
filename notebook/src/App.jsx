d9e7f042b2be363a56791fa63fd2437d99d4323f
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-04-13 23:27:37 +0300

    add login and authorize creating new note

--- notebook/src/App.jsx
+++ notebook/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import noteService from "./services/notes"
+import login from './services/login'
 import Notification from './components/Notification'
 import { Footer } from './components/Footer'
 import { Note } from './components/Note'
@@ -10,6 +11,9 @@ export default function App() {
   const [newNote, setNewNote] = useState("")
   const [showALl, setShowAll] =useState(false)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(()=>{
     noteService
@@ -18,11 +22,32 @@ export default function App() {
         setNotes(initialNotes)})
   },[])
 
-const showError =(text)=>{
-  setErrorMessage(text)
+  const showError =(text)=>{
+    setErrorMessage(text)
 
-  setTimeout(()=>setErrorMessage(null),4000)
-}
+    setTimeout(()=>setErrorMessage(null),4000)
+  }
+  const handleNoteChange =event => setNewNote(event.target.value)
+  const handleLogin = async(event) => {
+    event.preventDefault()
+    console.log(`Logging ${username} with password ${password}`)
+
+    const newUser = {
+      username,
+      password
+    }
+    try {
+      const savedUser = await login(newUser)
+      setUser(savedUser)
+      noteService.setToken(savedUser.token)
+      setUserName('')
+      setPassword('')
+    } catch (error) {
+      showError(error.response?.data?.error)
+    }
+
+    
+  }
 
   const addNote = (event)=>{
     event.preventDefault()
@@ -35,7 +60,7 @@ const showError =(text)=>{
       important: Math.random() <0.5
     }
     noteService
-      .create(noteObject)
+      .create(noteObject, user.token)
       .then(newObject=>{
         setNotes(notes.concat(newObject))
         setNewNote("")
@@ -43,9 +68,7 @@ const showError =(text)=>{
       .catch(err=>{
         showError(err.response?.data?.error)
       })
-  }
-  const handleNoteChange =event => setNewNote(event.target.value)
-  
+  }  
   const toggleImportanceOf =(id)=>{
     const note= notes.find(note => note.id ===id)
     if(!note) return 
@@ -73,18 +96,54 @@ const showError =(text)=>{
 
   const notesToShow = showALl? notes : notes.filter(note => note.important)
 
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input placeholder='add a new note...' value={newNote} onChange={handleNoteChange}/>
+      <button type='submit'>Save</button>
+    </form>
+  )
+
+  const loginForm = () => {
+    return <form onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="Username">Username: </label>
+        <input type="text" value={username}
+          onChange={({target}) => setUserName(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="password" 
+          value={password} 
+          onChange={({target}) => setPassword(target.value)} 
+        />
+      </div>
+      <button type='submit' style={{backgroundColor: 'light-green'}}>Login</button>
+    </form>
+  }
   return (
     <div className='note-container'>
       <h1>Notes</h1>
       <Notification message={errorMessage}/>
+      
+      {
+        user === null 
+          ? <>
+              <h2>Login</h2>
+             { loginForm()}
+            </>
+          : <>
+              <h2>{user.name} logged-in</h2>
+              {noteForm()}
+            </>    
+      }
+      
       <button onClick={()=> setShowAll(!showALl)}>Show {showALl ? "important" : "all"}</button>
       <ul>
         {notesToShow.map(note =><Note key={note.id} note={note} toggleImportance={()=>toggleImportanceOf(note.id)}/>)}
       </ul>
-      <form onSubmit={addNote}>
-        <input placeholder='add a new note...' value={newNote} onChange={handleNoteChange}/>
-        <button type='submit'>Save</button>
-      </form>
+     
       <Footer/>
     </div>
   )
--- notebook/src/services/notes.js
+++ notebook/src/services/notes.js
@@ -2,19 +2,28 @@ import axios from "axios"
 
 const baseUrl = "/api/notes"
 
+let token = null
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll =async ()=>{
     const request = axios.get(baseUrl)
     const res = await request
     const nonExistingObj ={
-        id:1000,
-        content: "this does not exist in server",
-        important: "false"
+      id:1000,
+      content: "this does not exist in server",
+      important: "false"
     }
     return res.data.concat(nonExistingObj)
 }
 
 const create =(newObject)=>{
-    const request = axios.post(baseUrl, newObject)
+    const config = {
+      headers: {'Authorization': token}
+    }
+    const request = axios.post(baseUrl, newObject, config)
+        
     return request.then(res => res.data)
 }
     
@@ -23,4 +32,5 @@ const update =async (id, newObject)=>{
     return res.data
 }
 
-export default {getAll, create, update}
+
+export default {getAll, create, update, setToken}
