d4f5cb53c68e30c88050c1c496197692d9914e9e
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-04-14 00:57:46 +0300

    handle json web token error in error handler middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -34,6 +34,9 @@ const errorHandler= (error, req, res, next) => {
   if(error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')){
     return res.status(400).json({ error: 'expected `username` to be unique' })
   }
+  if(error.name === 'JsonWebTokenError'){
+    return res.status(401).json({ error: 'Invalid token' })
+  }
   next(error)
 }
 
