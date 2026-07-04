e412d3bf4e4aec7f2f366e1b8616b42c5bb08aa9
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-07-04 22:38:54 +0300

    Fix: collect backend port

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,7 @@ const unknownEndpoint = (req,res)=>{
 
 app.use(unknownEndpoint)
 
-const PORT =3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
   console.log("Server running on port", PORT)
 })
