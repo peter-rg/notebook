63ed266cd20ffb0f92f12eb03b35d0b01757f349
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-08-01 21:14:15 +0300

    feat: add database functionality for creating and finding note

---
+++ backend/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length <3){
+  console.log("give password as an argument");
+  process.exit(1)  
+}
+
+const password = process.argv[2]
+
+const mongo_url = `mongodb+srv://rymerkih:${password}@cluster0.1zqhghm.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+// const mongo_url = 'mongodb://127.0.0.1:27017/noteApp'
+
+mongoose.set('strictQuery', false)
+mongoose.connect(mongo_url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: "Caro is bothering me!",
+  important: true,
+})
+
+note.save().then(result=>{
+  console.log("note saved");
+  mongoose.connection.close()  
+})
+
+
+       //async practice 
+// const findNotes = async()=>{
+//   try {
+//     const notes = await Note.find({})    
+//     notes.forEach(note=> console.log(note));
+    
+//   } catch (error) {
+//     console.log("An error ocuured", error);    
+//   }finally{
+//     mongoose.connection.close()
+//   }
+// }
+// findNotes()
+
+Note.find({}).then(result=>{
+  result.forEach(note=>{
+    console.log('note', note);
+  })
+  mongoose.connection.close()
+})
